117d150f6074a327e29dd1c1b6f9264b6b53538b
Robert Audi <robert.audii@gmail.com>
2019-05-07 17:22:14 +0200

    Add some tests for the ZohoCRM::Fields::Field and ZohoCRM::Model classes

--- spec/zoho_crm/fields/field_spec.rb
+++ spec/zoho_crm/fields/field_spec.rb
@@ -2,34 +2,347 @@
 
 RSpec.describe ZohoCRM::Fields::Field do
   describe ZohoCRM::Fields::Field::UnsupportedMethodTypeError do
-    pending "TODO: Write some tests"
+    it "requires an object", aggregate_failures: true do
+      expect { described_class.new }.to raise_error(ArgumentError, /wrong number of arguments/)
+      expect { described_class.new(nil) }.to_not raise_error
+    end
+
+    it "is a kind of TypeError" do
+      expect(described_class.new(nil)).to be_kind_of(TypeError)
+    end
+
+    # Note: this is a terrible description...
+    it "has a descriptive message" do
+      object = 42
+      error = described_class.new(object)
+
+      expect(error.message).to match(/expected to receive one of \[.+\] but received a #{object.class.name}: #{object.inspect}/)
+    end
+  end
+
+  describe "Attributes" do
+    subject { described_class.new(:email) }
+
+    it { is_expected.to have_attr_reader(:name) }
+    it { is_expected.to have_attr_reader(:api_name) }
+    it { is_expected.to have_attr_reader(:label) }
+    it { is_expected.to have_attr_reader(:field_method) }
+    it { is_expected.to have_attr_reader(:options) }
   end
 
   describe ".build" do
-    pending "TODO: Write some tests"
+    context "with a :values key in the options" do
+      subject(:field) { described_class.build(:status, values: %i[enabled disabled]) }
+
+      it "builds a field of type ZohoCRM::Fields::Enum" do
+        expect(field).to be_an_instance_of(ZohoCRM::Fields::Enum)
+      end
+    end
+
+    context "without a :values key in the options" do
+      subject(:field) { described_class.build(:email) }
+
+      it "builds a new field" do
+        expect(field).to be_an_instance_of(described_class)
+      end
+    end
+  end
+
+  describe "#initialize" do
+    it "normalizes the options", aggregate_failures: true do
+      field = described_class.new(:email, foo: 42)
+
+      expect(field.options.key?(:foo)).to be(false)
+      expect(field.options.key?("foo")).to be(true)
+    end
+
+    it 'requires a "field_name"', aggregate_failures: true do
+      expect { described_class.new }.to raise_error(ArgumentError, /wrong number of arguments/)
+      expect { described_class.new(:email) }.to_not raise_error
+    end
+
+    context 'when the "field_name" is blank' do
+      it "raises an error", aggregate_failures: true do
+        expect { described_class.new(false) }.to raise_error(ArgumentError, "name can't be blank")
+        expect { described_class.new(nil) }.to raise_error(ArgumentError, "name can't be blank")
+        expect { described_class.new("") }.to raise_error(ArgumentError, "name can't be blank")
+        expect { described_class.new(" ") }.to raise_error(ArgumentError, "name can't be blank")
+      end
+    end
+
+    context "with options" do
+      subject(:field) { described_class.new(:full_name, :name, as: "Full_Name", label: "Full Name (First Name + Last Name)") }
+
+      it "sets the field's attributes" do
+        expect(field).to have_attributes({
+          name: "full_name",
+          api_name: "Full_Name",
+          field_method: :name,
+          label: "Full Name (First Name + Last Name)",
+          options: {},
+        })
+      end
+
+      it 'deletes the "api_name" and "label" keys', aggregate_failures: true do
+        expect(field.options.key?("api_name")).to be(false)
+        expect(field.options.key?("label")).to be(false)
+      end
+    end
+
+    context "without options" do
+      subject(:field) { described_class.new(:first_name) }
+
+      it "sets field's #name" do
+        expect(field.name).to eq("first_name")
+      end
+
+      it "infers the #api_name from the field's #name" do
+        expect(field.api_name).to eq("First_Name")
+      end
+
+      it "sets the #field_method to the field's #name" do
+        expect(field.field_method).to eq("first_name")
+      end
+
+      it "infers the #label from the field's #api_name" do
+        expect(field.label).to eq("First Name")
+      end
+    end
+
+    context "when used with a block" do
+      it "uses the block as the #field_method" do
+        field_method = proc { 10 }
+        field = described_class.new(:age, &field_method)
+
+        expect(field.field_method).to eq(field_method)
+      end
+
+      it 'takes precedence over the "field_method" argument' do
+        field_method = proc { "Sabine Jelahaie" }
+        field = described_class.new(:name, :full_name, &field_method)
+
+        expect(field.field_method).to eq(field_method)
+      end
+    end
+
+    context "when the #field_method is of an unsupported type" do
+      it "raises an error" do
+        field_method = true
+
+        expect { described_class.new(:enabled, field_method) }
+          .to raise_error(described_class::UnsupportedMethodTypeError, /expected to receive .+ but received a #{field_method.class.name}: #{field_method.inspect}/)
+      end
+    end
   end
 
   describe "#api_name=" do
-    pending "TODO: Write some tests"
+    context "when a blank value is assigned" do
+      subject(:field) { described_class.new(:name) }
+
+      it "uses the inferred api_name", aggregate_failures: true do
+        field.api_name = nil
+
+        expect(field.api_name).to eq("Name")
+        expect { field.api_name = false }.to_not change(field, :api_name)
+        expect { field.api_name = "" }.to_not change(field, :api_name)
+        expect { field.api_name = " " }.to_not change(field, :api_name)
+      end
+    end
+
+    context "when a non-blank value is assigned" do
+      subject(:field) { described_class.new(:name) }
+
+      it "sets the #api_name to the given value" do
+        field.api_name = "Full_Name"
+
+        expect(field.api_name).to eq("Full_Name")
+      end
+
+      it "converts the value to a String" do
+        field.api_name = :Full_Name
+
+        expect(field.api_name).to eq("Full_Name")
+      end
+
+      it "strips the the whitespace from the value before assigning it" do
+        field.api_name = "   Full_Name   "
+
+        expect(field.api_name).to eq("Full_Name")
+      end
+    end
   end
 
   describe "#label=" do
-    pending "TODO: Write some tests"
+    context "when a blank value is assigned" do
+      subject(:field) { described_class.new(:name) }
+
+      it "is inferred from #api_name" do
+        field.api_name = "Full_Name"
+        field.label = nil
+
+        expect(field.label).to eq("Full Name")
+      end
+    end
+
+    context "when a non-blank value is assigned" do
+      subject(:field) { described_class.new(:email) }
+
+      it "sets the #api_name to the given value" do
+        field.label = "Email Address"
+
+        expect(field.label).to eq("Email Address")
+      end
+
+      it "strips the the whitespace from the value before assigning it" do
+        field.label = "   Email Address   "
+
+        expect(field.label).to eq("Email Address")
+      end
+    end
   end
 
   describe "#value_for" do
-    pending "TODO: Write some tests"
+    context "when the #field_method is a String" do
+      before do
+        MyUser = Struct.new(:full_name)
+      end
+
+      after do
+        Object.send(:remove_const, :MyUser)
+      end
+
+      it "calls the method of the same name on the object" do
+        field = described_class.new(:name, "full_name")
+        object = MyUser.new("Sabine Jelahaie")
+
+        expect(field.value_for(object)).to eq(object.full_name)
+      end
+    end
+
+    context "when the #field_method is a Symbol" do
+      before do
+        MyUser = Struct.new(:full_name)
+      end
+
+      after do
+        Object.send(:remove_const, :MyUser)
+      end
+
+      it "calls the method of the same name on the object" do
+        field = described_class.new(:name, :full_name)
+        object = MyUser.new("Sabine Jelahaie")
+
+        expect(field.value_for(object)).to eq(object.full_name)
+      end
+    end
+
+    context "when the #field_method is a Proc" do
+      before do
+        MyUser = Struct.new(:first_name, :last_name, keyword_init: true)
+      end
+
+      after do
+        Object.send(:remove_const, :MyUser)
+      end
+
+      it "executes the Proc in the context of the object", aggregate_failures: true do
+        field1 = described_class.new(:name) { |obj| "#{obj.last_name}, #{obj.first_name} #{obj.last_name}" }
+        field2 = described_class.new(:name) { "#{first_name} ! #{first_name} ! #{first_name} #{last_name}" }
+        object = MyUser.new(first_name: "Sabine", last_name: "Jelahaie")
+
+        expect(field1.value_for(object)).to eq("#{object.last_name}, #{object.first_name} #{object.last_name}")
+        expect(field2.value_for(object)).to eq("#{object.first_name} ! #{object.first_name} ! #{object.first_name} #{object.last_name}")
+      end
+    end
   end
 
   describe "#enum?" do
-    pending "TODO: Write some tests"
+    subject(:field) { described_class.new(:email) }
+
+    it "returns false" do
+      expect(field).to_not be_enum
+    end
   end
 
   describe "#hash" do
-    pending "TODO: Write some tests"
+    subject(:field) { described_class.new(:email) }
+
+    it "is the same as the hash of its :name attribute" do
+      expect(field.hash).to eq(field.name.hash)
+    end
+  end
+
+  describe "#eql?" do
+    context "when the other value is a ZohoCRM::Fields::Field" do
+      context "when the other field has the same name" do
+        it "returns true" do
+          field = described_class.new(:email)
+          other_field = described_class.new(:email)
+
+          expect(field.eql?(other_field)).to be(true)
+        end
+      end
+
+      context "when the other field has a different name" do
+        it "returns false" do
+          field = described_class.new(:email)
+          other_field = described_class.new(:name)
+
+          expect(field.eql?(other_field)).to be(false)
+        end
+      end
+    end
+
+    context "when the other value is a Symbol" do
+      context "when the symbol is the same as the field's name" do
+        it "returns true" do
+          field = described_class.new(:email)
+
+          expect(field.eql?(:email)).to be(true)
+        end
+      end
+
+      context "when the symbol is different than the field's name" do
+        it "returns false" do
+          field = described_class.new(:email)
+
+          expect(field.eql?(:name)).to be(false)
+        end
+      end
+    end
+
+    context "when the other value is a String" do
+      context "when the symbol is the same as the field's name" do
+        it "returns true" do
+          field = described_class.new(:email)
+
+          expect(field.eql?("email")).to be(true)
+        end
+      end
+
+      context "when the symbol is different than the field's name" do
+        it "returns false" do
+          field = described_class.new(:email)
+
+          expect(field.eql?("name")).to be(false)
+        end
+      end
+    end
+
+    context "when the other value is of an unsupported type" do
+      it "returns false" do
+        field = described_class.new(:leet)
+
+        expect(field.eql?(1337)).to be(false)
+      end
+    end
   end
 
   describe "#inspect" do
-    pending "TODO: Write some tests"
+    subject(:field) { described_class.new(:email, :email_address, as: "Email") }
+
+    it "returns a human-readable representation of the field" do
+      expect(field.inspect).to match(/\A#<#{described_class.name} name: "email" api_name: "Email" field_method: :email_address options: {}>\z/)
+    end
   end
 end
--- spec/zoho_crm/model_spec.rb
+++ spec/zoho_crm/model_spec.rb
@@ -1,47 +1,297 @@
 # frozen_string_literal: true
 
 RSpec.describe ZohoCRM::Model do
+  describe "Attributes" do
+    it "has :zoho_module_name class-level attribute reader" do
+      model = Class.new(described_class)
+
+      expect(model).to have_attr_reader(:zoho_module_name)
+    end
+
+    it "has a :object attribute reader" do
+      model = Class.new(described_class)
+
+      expect(model.new(double)).to have_attr_reader(:object)
+    end
+  end
+
   describe ".zoho_module" do
-    pending "TODO: Write some tests"
+    it "requires an argument", aggregate_failures: true do
+      model = Class.new(described_class)
+
+      expect { model.zoho_module }.to raise_error(ArgumentError)
+      expect { model.zoho_module("name") }.to_not raise_error
+    end
+
+    it "sets the module name" do
+      model = Class.new(described_class) { zoho_module("my_module") }
+
+      expect(model.zoho_module_name).to eq("my_module")
+    end
+
+    context "when the given name is nil" do
+      it "sets the module name to nil" do
+        model = Class.new(described_class) { zoho_module(nil) }
+
+        expect(model.zoho_module_name).to be_nil
+      end
+    end
+
+    context "when the given name is empty" do
+      it "sets the module name to nil" do
+        model = Class.new(described_class) { zoho_module("") }
+
+        expect(model.zoho_module_name).to be_nil
+      end
+    end
+
+    context "when the given name is a Symbol" do
+      it "sets the module name to the String representation of the given name" do
+        model = Class.new(described_class) { zoho_module(:my_module) }
+
+        expect(model.zoho_module_name).to eq("my_module")
+      end
+    end
   end
 
   describe ".zoho_fields" do
-    pending "TODO: Write some tests"
-  end
+    it "is a FieldSet" do
+      model = Class.new(described_class)
 
-  describe ".fields" do
-    pending "TODO: Write some tests"
+      expect(model.zoho_fields).to be_a(ZohoCRM::FieldSet)
+    end
   end
 
   describe ".zoho_field" do
-    pending "TODO: Write some tests"
+    it "requires a name as argument", aggregate_failures: true do
+      model = Class.new(described_class)
+
+      expect { model.zoho_field }.to raise_error(ArgumentError)
+      expect { model.zoho_field(:email) }.to_not raise_error
+    end
+
+    it "defines a method with the given name" do
+      model = Class.new(described_class) { zoho_field(:first_name) }
+
+      expect(model.method_defined?(:first_name)).to be(true)
+    end
+
+    it "adds a zoho field", aggregate_failures: true do
+      model = Class.new(described_class)
+
+      expect { model.zoho_field(:first_name) }.to change { model.zoho_fields.size }.by(1)
+      expect(model.zoho_fields).to include(:first_name)
+    end
   end
 
   describe ".field" do
-    pending "TODO: Write some tests"
+    context "when the field exists" do
+      subject(:model) { Class.new(described_class) { zoho_field(:first_name) } }
+
+      it "returns the field", aggregate_failures: true do
+        field = model.field(:first_name)
+
+        expect(field).to be_a(ZohoCRM::Fields::Field)
+        expect(field.name).to eql("first_name")
+      end
+    end
+
+    context "when the field does not exist" do
+      subject(:model) { Class.new(described_class) }
+
+      it "raises an error", aggregate_failures: true do
+        expect { model.field(:first_name) }.to raise_error(ZohoCRM::FieldSet::FieldNotFoundError) do |error|
+          expect(error.field_name).to eq(:first_name)
+          expect(error.fields).to eq(model.zoho_fields)
+          expect(error.message).to eq("Field not found: first_name")
+        end
+      end
+    end
   end
 
   describe ".field?" do
-    pending "TODO: Write some tests"
+    context "when the field exists" do
+      subject(:model) { Class.new(described_class) { zoho_field(:first_name) } }
+
+      it "returns true" do
+        expect(model.field?(:first_name)).to be(true)
+      end
+    end
+
+    context "when the field does not exist" do
+      subject(:model) { Class.new(described_class) }
+
+      it "returns false" do
+        expect(model.field?(:first_name)).to be(false)
+      end
+    end
   end
 
   describe ".inspect" do
-    pending "TODO: Write some tests"
+    before do
+      MyZohoModel = Class.new(described_class) {
+        zoho_module("CustomModule")
+        zoho_field(:email)
+      }
+    end
+
+    after do
+      Object.send(:remove_const, :MyZohoModel)
+    end
+
+    it "returns a human-readable representation of the model class" do
+      expect(MyZohoModel.inspect).to match(/\A#<MyZohoModel < #{described_class.name} zoho_module_name: "CustomModule" fields: .+>\z/)
+    end
+  end
+
+  describe "#initialize" do
+    it "requires an object as argument", aggregate_failures: true do
+      model = Class.new(described_class)
+
+      expect { model.new }.to raise_error(ArgumentError)
+      expect { model.new(Object.new) }.to_not raise_error
+    end
+
+    it "sets the :object attribute" do
+      model = Class.new(described_class)
+      object = Object.new
+      instance = model.new(object)
+
+      expect(instance.object).to eq(object)
+    end
+  end
+
+  describe "#zoho_fields" do
+    subject(:model) { Class.new(described_class) { zoho_field(:first_name) } }
+
+    it "is delegated to the class" do
+      instance = model.new(Object.new)
+
+      expect(instance.zoho_fields).to eq(model.zoho_fields)
+    end
+  end
+
+  describe "#field" do
+    subject(:model) { Class.new(described_class) { zoho_field(:first_name) } }
+
+    it "is delegated to the class" do
+      instance = model.new(Object.new)
+
+      expect(instance.field(:first_name)).to eq(model.field(:first_name))
+    end
+  end
+
+  describe "#field?" do
+    subject(:model) { Class.new(described_class) { zoho_field(:first_name) } }
+
+    it "is delegated to the class" do
+      instance = model.new(Object.new)
+
+      expect(instance.field?(:first_name)).to eq(model.field?(:first_name))
+    end
   end
 
   describe "#zoho_module" do
-    pending "TODO: Write some tests"
+    context "when there is a :zoho_module_name" do
+      it "returns the value of :zoho_module_name" do
+        MyZohoModel = Class.new(described_class) { zoho_module("ZohoStuff") }
+        model = MyZohoModel.new(double)
+
+        expect(model.zoho_module).to eq("ZohoStuff")
+
+        Object.send(:remove_const, :MyZohoModel)
+      end
+    end
+
+    context "when there is no :zoho_module_name" do
+      it "returns a String representation of the class name" do
+        MyZohoModel = Class.new(described_class)
+        model = MyZohoModel.new(double)
+
+        expect(model.zoho_module).to eq("MyZohoModel")
+
+        Object.send(:remove_const, :MyZohoModel)
+      end
+
+      it "returns the class name without its namespacing" do
+        MyZohoNamespace = Module.new
+        MyZohoNamespace::MyZohoModel = Class.new(described_class)
+        model = MyZohoNamespace::MyZohoModel.new(double)
+
+        expect(model.zoho_module).to eq("MyZohoModel")
+
+        MyZohoNamespace.send(:remove_const, :MyZohoModel)
+        Object.send(:remove_const, :MyZohoNamespace)
+      end
+    end
   end
 
   describe "#as_json" do
-    pending "TODO: Write some tests"
+    before do
+      MyZohoModel = Class.new(described_class) {
+        zoho_field(:email, as: "Email")
+        zoho_field(:full_name, as: "Full_Name")
+      }
+
+      MyUser = Struct.new(:email, :full_name, keyword_init: true)
+    end
+
+    after do
+      Object.send(:remove_const, :MyZohoModel)
+      Object.send(:remove_const, :MyUser)
+    end
+
+    it "returns the JSON representation of model as a Ruby Hash" do
+      user = MyUser.new(email: "user@example.com", full_name: "Yoo Zerr")
+      instance = MyZohoModel.new(user)
+
+      expect(instance.as_json).to be_a(Hash).and eq({
+        "Email" => "user@example.com",
+        "Full_Name" => "Yoo Zerr",
+      })
+    end
   end
 
   describe "#to_json" do
-    pending "TODO: Write some tests"
+    before do
+      MyZohoModel = Class.new(described_class) {
+        zoho_field(:email, as: "Email")
+        zoho_field(:full_name, as: "Full_Name")
+      }
+
+      MyUser = Struct.new(:email, :full_name, keyword_init: true)
+    end
+
+    after do
+      Object.send(:remove_const, :MyZohoModel)
+      Object.send(:remove_const, :MyUser)
+    end
+
+    it "returns the JSON representation of model" do
+      user = MyUser.new(email: "user@example.com", full_name: "Yoo Zerr")
+      instance = MyZohoModel.new(user)
+
+      expect(instance.to_json).to be_a(String).and eq('{"Email":"user@example.com","Full_Name":"Yoo Zerr"}')
+    end
   end
 
   describe "#inspect" do
-    pending "TODO: Write some tests"
+    before do
+      MyZohoModel = Class.new(described_class) {
+        zoho_module("CustomModule")
+        zoho_field(:string, :to_s)
+      }
+    end
+
+    after do
+      Object.send(:remove_const, :MyZohoModel)
+    end
+
+    it "returns a human-readable representation of the model class" do
+      object = Object.new
+      instance = MyZohoModel.new(object)
+
+      expect(instance.inspect).to match(/\A#<MyZohoModel:#{instance.object_id} zoho_module: "CustomModule" object: #{object.inspect} fields: .+>\z/)
+    end
   end
 end
