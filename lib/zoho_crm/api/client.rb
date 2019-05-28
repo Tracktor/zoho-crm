3247dfff64214a8b312decaaadbeb58e5a5080c5
Robert Audi <robert.audii@gmail.com>
2019-05-28 17:32:49 +0200

    Update the YARD doc on the ZohoCRM::API::Client class

--- lib/zoho_crm/api/client.rb
+++ lib/zoho_crm/api/client.rb
@@ -3,30 +3,57 @@
 module ZohoCRM
   module API
     class Client < Connection
-      # Zoho CRM API Docs: {https://www.zoho.com/crm/help/developer/api/get-specific-record.html Get a specific record}
+      # Get a record
+      #
+      # @see https://www.zoho.com/crm/help/developer/api/get-specific-record.html
+      #
+      # @param record_id [String] the ID of the record
+      # @param module_name [String] the name of the Zoho module
       def show(record_id, module_name:)
         get("#{module_name}/#{record_id}")
       end
 
-      # Zoho CRM API Docs: {https://www.zoho.com/crm/help/developer/api/insert-records.html Insert records}
-      def create(records, module_name:)
-        post(module_name, body: build_body(records))
+      # Add a new record to a module.
+      #
+      # @see https://www.zoho.com/crm/help/developer/api/insert-records.html
+      #
+      # @param attributes [Array, Hash] Record attributes
+      # @param module_name [String] the name of the Zoho module
+      def create(attributes, module_name:)
+        post(module_name, body: build_body(attributes))
       end
 
-      # Zoho CRM API Docs: {https://www.zoho.com/crm/help/developer/api/update-specific-record.html Update a specific record}
-      def update(record_id, body, module_name:)
-        put("#{module_name}/#{record_id}", body: build_body(body))
+      # Update a record
+      #
+      # @see https://www.zoho.com/crm/help/developer/api/update-specific-record.html
+      #
+      # @param record_id [String] the ID of the record
+      # @param attributes [Hash] the new record attributes
+      # @param module_name [String] the name of the Zoho module
+      def update(record_id, attributes, module_name:)
+        put("#{module_name}/#{record_id}", body: build_body(attributes))
       end
 
-      # Zoho CRM API Docs: {https://www.zoho.com/crm/help/developer/api/upsert-records.html Insert or Update records}
-      def upsert(records, module_name:, duplicate_check_fields: [])
-        body = build_body(records)
+      # Insert or Update a record
+      #
+      # @see https://www.zoho.com/crm/help/developer/api/upsert-records.html
+      #
+      # @param attributes [Hash] the record attributes
+      # @param module_name [String] the name of the Zoho module
+      # @param duplicate_check_fields [Array] list of fields to check against existing records
+      def upsert(attributes, module_name:, duplicate_check_fields: [])
+        body = build_body(attributes)
         body[:duplicate_check_fields] = Array(duplicate_check_fields).join(",")
 
         post("#{module_name}/upsert", body: body)
       end
 
-      # Zoho CRM API Docs: {https://www.zoho.com/crm/help/developer/api/delete-specific-record.html Delete a specific record}
+      # Delete a record
+      #
+      # @see https://www.zoho.com/crm/help/developer/api/delete-specific-record.html
+      #
+      # @param record_id [String] the ID of the record
+      # @param module_name [String] the name of the Zoho module
       def destroy(record_id, module_name:)
         delete("#{module_name}/#{record_id}")
       end
