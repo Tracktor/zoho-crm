44a8d8d38097accf71e1b55139d1140982c0eb79
Robert Audi <robert.audii@gmail.com>
2019-08-29 09:06:50 +0200

    Add a negated RSpec matcher for the `change` matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "bundler/setup"
 require "zoho_crm"
 
 require_relative "support/matchers/have_attr"
+require_relative "support/matchers/not_change"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
